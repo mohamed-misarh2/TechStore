074b621b43c6e40f1ec536fbdf724fe37dc14781
mohamed-misarh2 <Mohamedmisarh2@gmail.com>
2024-03-19 22:49:43 +0200

    ediiiiit

--- TechStore.Application/Mapper/AutoMapperProfile.cs
+++ TechStore.Application/Mapper/AutoMapperProfile.cs
@@ -9,31 +9,24 @@ using TechStore.Dtos;
 using TechStore.Models;
 using TechStore.Dtos.UserDTO;
 using TechStore.Dtos.CategoryDtos;
-using TechStore.Dtos.AccountDtos;
 using Microsoft.AspNetCore.Identity;
 using TechStore.Dtos.ReviewDtos;
 
 
 namespace TechStore.Application.Mapper
 {
-    internal class AutoMapperProfile :Profile
+    public class AutoMapperProfile :Profile
     {
-        public AutoMapperProfile() 
+        public AutoMapperProfile()
         {
             CreateMap<UpdateUserDTO, TechUser>().ReverseMap();
             CreateMap<GetAllUserDTO, TechUser>().ReverseMap();
             CreateMap<LoginDto, TechUser>().ReverseMap();
             CreateMap<RegisterDto, TechUser>().ReverseMap();
-            CreateMap<CreateOrUpdateCategory,Category>().ReverseMap();
-            CreateMap<GetAllCategory,Category>().ReverseMap();
-            CreateMap<CreateOrUpdateReviewDto, Review>().ReverseMap();
-            CreateMap<GetAllReviewDto, Review>().ReverseMap();
 
-            CreateMap<RegisterDto, TechUser>().ReverseMap();
-            CreateMap<LoginDto, TechUser>().ReverseMap();
             CreateMap<UserDto, TechUser>().ReverseMap();
             CreateMap<CategoryDto, Category>().ReverseMap();
-            CreateMap<CreateOrUpdateReviewDto, Review>().ReverseMap();
+            CreateMap <CreateOrUpdateReviewDto, Review>().ReverseMap();
             CreateMap<GetAllReviewDto, Review>().ReverseMap();
            
         }
