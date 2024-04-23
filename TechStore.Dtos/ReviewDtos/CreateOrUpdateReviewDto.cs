2acc6058a75fff28853ca583d160f665f369aa3a
Mahmoud Abdelmoaty <mahmoudabdelmoaty010@gmail.com>
2024-04-23 02:49:21 +0200

    ddssfsf

--- TechStore.Dtos/ReviewDtos/CreateOrUpdateReviewDto.cs
+++ TechStore.Dtos/ReviewDtos/CreateOrUpdateReviewDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,10 @@ namespace TechStore.Dtos.ReviewDtos
         public int Id { get; set; }
         public string UserId { get; set; }
         public int ProductId { get; set; }
+        [Required(ErrorMessage = "Rating is Required")]
         public int Rating { get; set; }
+
+        [Required(ErrorMessage = "Comment is Required")]
         public string Comment { get; set; }
         public DateTime ReviewDate { get; set; }
 
--- TechStore.ViewUser/wwwroot/js/Cart.js
+++ TechStore.ViewUser/wwwroot/js/Cart.js
@@ -212,35 +212,3 @@ $(document).ready(function () {
 
 // var cartItems = JSON.parse(localStorage.getItem('mycartapp')) || [];
 
-//const swiper = new Swiper('.swiper', {
-
-//    loop: true,
-
-//    pagination: {
-//        el: '.swiper-pagination',
-//    },
-
-
-//    navigation: {
-//        nextEl: '.swiper-button-next',
-//        prevEl: '.swiper-button-prev',
-//    },
-
-//    slidesPerView: 1,
-//    spaceBetween: 10,
-//    breakpoints: {
-//        320: {
-//            slidesPerView: 1,
-//            spaceBetween: 10
-//        },
-//        480: {
-//            slidesPerView: 2,
-//            spaceBetween: 20
-//        },
-//        640: {
-//            slidesPerView: 5,
-//            spaceBetween: 10
-//        }
-//    }
-//});
-//window.alert(swiper)
